d5695ca33ec82f953e30d88b65441c46d38af87e
ichrak guesmi <guesmiichrak3@gmail.com>
2020-10-11 13:46:19 +0200

    checkpointreact2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./style.css";
+import VideoPlayer from "react-video-js-player";
+import  myVideo from "./myVideo.mp4";
+import image from "./imageInSrc.jpg";
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+      <div style={{ border:"solid 1px black", maxWidth: '100vw'}}/>
+        <h1 className='titleRed' >Ichrak Guesmi</h1>
+        <br />
+       <img className="imagee" src={image} alt="imaInSrc"/>
+       <br />
+        <img className="imagee" src="/images/imageInpublic.jpg" alt="imageInpublic" />
+        
+        <br />
+      
+      <VideoPlayer className='video-js' 
+      src={myVideo}
+      poster={"./myVideo.mp4"}  width="320" height="240" controls= "controls" playBackRates={[0.5,1,3.85,16]} />
+     
+
     </div>
   );
-}
+ }
 
-export default App;
+export default App; 
